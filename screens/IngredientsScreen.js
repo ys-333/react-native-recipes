29f13850917ed654a46598b9971709b433fea701
ys-333 <yashwantsing333@gmail.com>
2023-02-06 22:29:49 +0530

    ingredient screen config

--- screens/IngredientsScreen.js
+++ screens/IngredientsScreen.js
@@ -1,6 +1,15 @@
-import { View, Image, Text, FlatList, StyleSheet } from 'react-native'
+import {
+  View,
+  Image,
+  Text,
+  FlatList,
+  StyleSheet,
+  ScrollView,
+  LogBox,
+} from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import MealDetails from '../components/MealDetails'
+import { useEffect } from 'react'
 
 function IngredientScreen({}) {
   const route = useRoute()
@@ -14,101 +23,107 @@ function IngredientScreen({}) {
     affordability,
   } = route.params
 
-  return (
-    <View style={styles.screen}>
-      <View
-        style={{
-          backgroundColor: 'white',
-          width: '90%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginLeft: 12,
-          borderRadius: 6,
-          elevation: 4,
-        }}
-      >
-        <Image
-          style={{
-            height: 200,
-            width: ' 90%',
-            marginVertical: 12,
-            marginLeft: 12,
-            borderRadius: 8,
-            borderWidth: 0.6,
-            borderColor: 'yellow',
-          }}
-          source={{ uri: imageUrl }}
-        />
-        <MealDetails
-          duration={duration}
-          complexity={complexity}
-          affordability={affordability}
-        />
-      </View>
-      <Text style={styles.title}>{title}</Text>
-      <View style={styles.ingredient}>
-        <Text style={styles.title}>Ingredients Screen</Text>
-        <FlatList
-          data={ingredients}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginLeft: 18,
-              }}
-            >
-              <Text
-                style={{
-                  marginRight: 8,
-                  width: 4,
-                  height: 4,
-                  backgroundColor: 'white',
-                  borderRadius: 14,
-                }}
-              ></Text>
-              <Text style={styles.list}>{item}</Text>
-            </View>
-          )}
-        />
-      </View>
+  useEffect(() => {
+    LogBox.ignoreLogs(['VirtualizedLists should never be nested'])
+  }, [])
 
-      <View style={styles.step}>
-        <Text style={styles.title}>Steps:</Text>
-        <FlatList
-          data={steps}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginLeft: 18,
-              }}
-            >
-              <Text
+  return (
+    <ScrollView style={{ flex: 1, marginBottom: 32 }}>
+      <View style={styles.screen}>
+        <View
+          style={{
+            backgroundColor: 'white',
+            width: '90%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginLeft: 12,
+            borderRadius: 6,
+            elevation: 4,
+          }}
+        >
+          <Image
+            style={{
+              height: 200,
+              width: ' 90%',
+              marginVertical: 12,
+              marginLeft: 12,
+              borderRadius: 8,
+              borderWidth: 0.6,
+              borderColor: 'yellow',
+            }}
+            source={{ uri: imageUrl }}
+          />
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
+        </View>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.ingredient}>
+          <Text style={styles.title}>Ingredients Screen</Text>
+          <FlatList
+            data={ingredients}
+            renderItem={({ item }) => (
+              <View
                 style={{
-                  marginRight: 8,
-                  width: 4,
-                  height: 4,
-                  backgroundColor: 'white',
-                  borderRadius: 14,
-                }}
-              ></Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  marginRight: 4,
-                  color: 'grey',
-                  marginBottom: 6,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginLeft: 18,
                 }}
               >
-                {item}
-              </Text>
-            </View>
-          )}
-        />
+                <Text
+                  style={{
+                    marginRight: 8,
+                    width: 4,
+                    height: 4,
+                    backgroundColor: 'white',
+                    borderRadius: 14,
+                  }}
+                ></Text>
+                <Text style={styles.list}>{item}</Text>
+              </View>
+            )}
+          />
+        </View>
+
+        <View style={styles.step}>
+          <Text style={styles.title}>Steps:</Text>
+          <FlatList
+            data={steps}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginLeft: 18,
+                }}
+              >
+                <Text
+                  style={{
+                    marginRight: 8,
+                    width: 4,
+                    height: 4,
+                    backgroundColor: 'white',
+                    borderRadius: 14,
+                  }}
+                ></Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    marginRight: 4,
+                    color: 'grey',
+                    marginBottom: 6,
+                  }}
+                >
+                  {item}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
