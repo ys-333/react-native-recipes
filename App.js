3f1538514d32b8b7d7e47a5284096f48637d999d
ys-333 <yashwantsing333@gmail.com>
2023-02-14 23:06:20 +0530

    nesting navigators done

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
+import 'react-native-gesture-handler'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import CategoriesScreen from './screens/CategoriesScreen'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import MealsOverviewScreen from './screens/MealsOveriewScreen'
 import IngredientScreen from './screens/IngredientsScreen'
+import FavoriteScreen from './screens/FavoriteScreen'
+import { Ionicons } from '@expo/vector-icons'
 
+const Drawer = createDrawerNavigator()
+const Stack = createNativeStackNavigator()
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#3f2f25',
+        },
+        drawerContentStyle: { backgroundColor: '#351401' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
+      }}
+    >
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{
+          title: 'Favorites',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  )
+}
 export default function App() {
-  const Stack = createNativeStackNavigator()
-
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -23,19 +66,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: 'Meal Category',
-              headerRight: () => {
-                return (
-                  <Button
-                    onPress={() => console.log('you pressed the tap button')}
-                    title="tap"
-                    color="black"
-                  />
-                )
-              },
+              headerShown: false,
             }}
           />
           <Stack.Screen
